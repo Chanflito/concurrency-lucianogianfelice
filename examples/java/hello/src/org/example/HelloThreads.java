85b11dc2f1505d0c16d8f58d0b3d29f262791e91
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-04-05 14:43:36 -0300

    resolution

--- examples/java/hello/src/org/example/HelloThreads.java
+++ examples/java/hello/src/org/example/HelloThreads.java
@@ -1,16 +1,46 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class HelloThreads {
-    private static void hello() {
-        var t1 = new Thread(() -> System.out.println("Hello from thread 1"));
+    private static Thread hello() throws InterruptedException {
+        var t1 = new Thread(() ->{
+            sleep(1000);
+            //printHello();
+        } );
         var t2 = new Thread(() -> System.out.println("Hello from thread 2"));
         t1.start();
         t2.start();
+        //t1.join();
+        t2.join();// T2 se hace join al hilo principal.
+        return t1;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("====== Hello Threads    =======");
-        hello();
+//        hello();
+        printHello20();
         System.out.println("====== Good Bye Threads =======");
+    }
+
+    private static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ignored) {
+        }
+    }
+    private static void printHello20() throws InterruptedException {
+        var threads=new ArrayList<Thread>();
+        for (int i = 0; i < 20; i++) {
+            //Create a thread
+            threads.add(new Thread(() -> System.out.println("Hello from thread " + Thread.currentThread().getId())));
+        }
+
+        for (var t : threads) {
+            t.start();
+        }
+        for (var t : threads) {
+            t.join();
+        }
     }
 }
---
+++ examples/java/hello/src/org/example/MatrixSum.java
@@ -0,0 +1,42 @@
+package org.example;
+
+
+public class MatrixSum {
+
+
+    public static Integer sumAllLines(int[][] matrix){
+        var threads=new ArrayList<Thread>();
+        int numOfThreads=matrix.length;
+        AtomicInteger finalResult= new AtomicInteger(0); // same problem as in rust?
+        //int[] sumOfEachRow= new int[numOfThreads]; Porque aca no me deja poner un acumulador tipo entero?
+        for (int i = 0; i < numOfThreads; i++) {
+            int rowIndex = i;
+            threads.add(new Thread( ()->{
+                int acumulator=0;
+                for (int j = 0; j < matrix[rowIndex].length; j++) {
+                    acumulator+=matrix[rowIndex][j];
+                }
+                System.out.println("Sum of line "+rowIndex+" is "+acumulator);
+//                sumOfEachRow[rowIndex]=acumulator;
+                finalResult.addAndGet(acumulator);
+            }));
+        }
+        for (var t : threads) {
+            t.start();
+        }
+        //This method ensures that the principal thread  (sumAllLines) waits until all threads are finished.
+        for (var t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+//        for (int sum : sumOfEachRow) {
+//            totalSum += sum;
+//        }
+        return finalResult.get();
+    }
+}
+
+
